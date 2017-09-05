0e740ecbe5c6e03f40eaf7056c3f91474a28709e
southwest59 <southwest59.14.1@gmail.com>
2017-09-05 19:41:48 +0900

    開発中一時保存

--- ingress/remaind_until_anomaly.py
+++ ingress/remaind_until_anomaly.py
@@ -1,18 +1,17 @@
-#アノマリー開催日までの残日数を算出
+# アノマリー開催日までの残日数を算出
 # coding:utf-8
 
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import pytz
 
-def remaind_until_anomaly():
+ANOMARY_DAY = '2017/11/04'
 
-    #anomary開催日
-    anomary_day = "2017/11/04"
+# TimeZone設定
+timezone = pytz.timezone('Asia/Tokyo')
 
-    #TimeZone設定
-    timezone = pytz.timezone('Asia/Tokyo')
+# 現在の日付
+now = datetime.now(tz=timezone)
+remaind_day = ANOMARY_DAY - now
 
-    #現在の日付
-    now = datetime.now(tz=timezone)
-
-    print("NowDate:" + now)
+#print("NowDate:" + now)
+#print('NowDate:' + datetime.now(tz=timezone))
