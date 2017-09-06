fe3af372f97fb5a7be999d8fdac86bdb9072e0fc
southwest59 <genyoshi@Gina.local>
2017-09-06 23:22:22 +0900

    開発中一時保存

--- ingress/cron_remaind_anomaly.py
+++ ingress/cron_remaind_anomaly.py
@@ -1,6 +1,11 @@
 # coding:utf-8
 # アノマリー開催日までの残日数を算出
 
-from datetime import datetime, timedelta
-import pytz
+from datetime import datetime
+from .property import TIME_ZONE, ANOMARY_DAY
 
+#現在の日付を取得(TimeZone起算)
+now = datetime.now(TIME_ZONE)
+
+print("anomary_day:" .format(ANOMARY_DAY))
+print("now:".format(now))
--- ingress/property.py
+++ ingress/property.py
@@ -1,18 +1,13 @@
 # coding:utf-8
 
-from datetime import date
-import pytz
+from datetime import date,timedelta
+from pytz import timezone
 
 #TimeZone
-TIME_ZONE = pytz.timezone('Asia/Tokyo')
+TIME_ZONE = timezone('Asia/Tokyo')
 
 #アノマリー開催日
 ANOMARY_DAY = date(2017,11,4)
 
-#現在
-#TODAY = date.today(TIME_ZONE)
-
-
 #test
 print( 'Anomary_day:' + date.strftime(ANOMARY_DAY,'%y-%m-%d') )
-print( TIME_ZONE)
