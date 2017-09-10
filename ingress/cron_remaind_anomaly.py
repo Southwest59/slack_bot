0123b75b3d5234047dd769380c027efb786e04dc
southwest59 <southwest59>
2017-09-10 20:35:49 +0900

    開発中一時保存

--- ingress/cron_remaind_anomaly.py
+++ ingress/cron_remaind_anomaly.py
@@ -6,23 +6,29 @@ from datetime import date,datetime,timezone,timedelta
 #Constants
 ANOMARY_DAY = date(2017,11,4)   #アノマリー開催日
 
-#残日数算出
-try:
-    today = date.today()
-    msg_rtn = ''
+def lambda_handler(event, context):
+    #残日数算出
+    try:
+        today = date.today()
+        msg_rtn = ''
 
-    if ANOMARY_DAY > today:
-        remaind = abs(ANOMARY_DAY - today)
+        if ANOMARY_DAY > today:
+            remaind = abs(ANOMARY_DAY - today)
 
-        msg_rtn = 'おはようございます。'
-        msg_rtn += '大阪アノマリーまであと' + str(remaind.days) + '日です。'
+            msg_rtn = "おはようございます。"
+            msg_rtn += "\n大阪アノマリーまであと" + str(remaind.days) + "日です。"
 
-        print(msg_rtn)
+            #print(msg_rtn)
 
-#elif ANOMARY_DAY == today:
+        elif ANOMARY_DAY == today:
+            msg_rtn = "おはようございます。"
+            msg_rtn += "大阪アノマリー当日です、Go!ENL!!"
+        else:
+            msg_rtn = "大阪アノマリーは終わったyo"
 
-#else:
+        payload = {'text' :msg_rtn}
+        return payload
 
-except Exception as e:
-    print(e)
-    raise e
+    except Exception as e:
+        print(e)
+        raise e
